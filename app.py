f80b26f1e66ac612f62f6ffb78c343ee5e530f63
Masooma Zia <115894895+Masooma82@users.noreply.github.com>
2023-08-09 20:00:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=use_static_image_mode,
-        max_num_hands=1,
+        max_num_hands=2,
         min_detection_confidence=min_detection_confidence,
         min_tracking_confidence=min_tracking_confidence,
     )
@@ -141,10 +141,10 @@ def main():
 
                 # Hand sign classification
                 hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                if hand_sign_id == 2:  # Point gesture
-                    point_history.append(landmark_list[8])
-                else:
-                    point_history.append([0, 0])
+                #if hand_sign_id == 2:  # Point gesture
+                    #point_history.append(landmark_list[8])
+                #else:
+                point_history.append([0, 0])
 
                 # Finger gesture classification
                 finger_gesture_id = 0
